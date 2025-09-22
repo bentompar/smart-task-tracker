196577e439a97e7322bd2a991b6424e29f5f18ee
Benjamin Park <bentompar@gmail.com>
2025-09-22 17:31:36 +0200

    chore: update pom.xml and main demo

--- src/main/java/com/BenjaminPark/demo/Main.java
+++ src/main/java/com/BenjaminPark/demo/Main.java
@@ -1,20 +1,29 @@
 package com.BenjaminPark.demo;
 
+import com.BenjaminPark.dto.TaskUpdateRequest;
+import com.BenjaminPark.model.Status;
 import com.BenjaminPark.model.Task;
 import com.BenjaminPark.model.User;
+import com.BenjaminPark.repository.TaskRepository;
+import com.BenjaminPark.service.DuplicateTaskException;
 import com.BenjaminPark.service.MissingTaskException;
 import com.BenjaminPark.service.TaskService;
 
 public class Main {
-    public static void main(String[] args) throws MissingTaskException {
-        User user = new User("Benjamin");
-        TaskService taskService = new TaskService();
+    public static void main(String[] args) throws MissingTaskException, DuplicateTaskException {
+        TaskUpdateRequest taskUpdateRequest = new TaskUpdateRequest();
+        User user = new User("Benjamin", "test".toCharArray());
+        TaskService taskService = new TaskService(new TaskRepository());
         Task task1 = new Task("Wash clothes", "Do the entire family's washing");
         Task task2 = new Task("Hoover apartment", "Hoover the entire apartment, including attic");
         Task task3 = new Task("Wash clothes", "Do only my washing");
+        taskUpdateRequest.setTitle("Wash clothes");
+        taskUpdateRequest.setDescription("Do only my washing");
+        taskUpdateRequest.setStatus(Status.IN_PROGRESS);
+
         taskService.addTask(user, task1);
         taskService.addTask(user, task2);
-        taskService.updateTask(user, task1.getTaskId(), task3);
+        taskService.updateTask(user, task1, taskUpdateRequest);
         taskService.deleteTask(user, task2.getTaskId());
 
         System.out.println(user.toString());
